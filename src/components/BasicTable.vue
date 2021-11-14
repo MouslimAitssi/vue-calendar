1f83d28f44ca93accea48eda62636339bddbf9dc
Mouslim Aitssi <mouslimaitssi@gmail.com>
2021-11-14 18:10:21 +0100

    calendar working

---
+++ src/components/BasicTable.vue
@@ -0,0 +1,167 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th></th>
+          <th v-for="day in days" :key="day" class="text-left">
+            {{day}}
+          </th>
+          <th>
+            Sélectionner tout
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="row in rows"
+          :key="row"
+        >
+          <td>{{row}}</td>
+          <td
+            v-for="day in days"
+            :key="row + day"
+            @click="toggle(row, day)"
+            v-bind:class="(isWeekend(day) ? 'gray-cell' : '') + ' ' + (getValue(row, day) !== ''&&!isWeekend(day) ? 'selected-cell' : '')"
+          >
+            {{ getValue(row, day) }}
+          </td>
+          <td @click="selectAll(row)"></td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+  export default {
+    name: 'BasicTable',
+    data () {
+      return {
+        //Number of days depends on month
+        NUMBER_OF_DAYS: 31,
+
+        days: [],
+
+        //The weekends are configurable here
+        weekends: [6, 7, 13, 14, 20, 21, 27, 28],
+
+        //The this.rows are configurable here
+        rows: [ 'Missions', 'Congés', 'Maladies' ],
+
+        values: [],
+
+      }
+    },
+    created () {
+        this.fillvaluesWithZeros();
+        for (let i = 1; i < this.NUMBER_OF_DAYS; i++) {
+          this.days.push(i);
+        }
+        console.log(this.values);
+      },
+    methods: { 
+      fillvaluesWithZeros() {
+        this.rows.map((row) => {
+          let arr = []
+          for (let i = 1; i < this.NUMBER_OF_DAYS; i++) {
+            arr.push(0)
+          }
+          this.values.push([row, arr]);
+        });
+      },
+      
+      getValue(row, day) {
+        if(!this.isWeekend(day)) {
+          for (let index = 0; index < this.values.length; index++) {
+            if(this.values[index][0] === row) {
+              if(this.values[index][1][day - 1] !==0) {
+                return this.values[index][1][day - 1];
+              }
+              // Rendering blank rather than 0
+              return '';
+            }
+          }
+
+          return '';
+        }
+      },
+
+      nextValue(prevValue) {
+        switch (prevValue) {
+          case 0:
+            return 1;
+          case 1:
+            return 0.5;
+          case 0.5:
+            return 1.5;
+          case 1.5:
+            return 0;
+          default:
+            return 0;
+        }
+      },
+      
+      toggle(row, day) {
+          let vals = Object.create(this.values);
+          for (let index = 0; index < vals.length; index++) {
+            if(vals[index][0] === row) {
+              vals[index][1][day - 1] = this.nextValue(vals[index][1][day - 1]);
+              break;
+            }
+          }
+          this.values = vals;
+      },
+
+      isWeekend(day) {
+        if (!this.weekends.includes(day)) {
+          return false;
+        }
+        return true;
+      },
+
+
+      selectAll(row) {
+        let vals = Object.create(this.values);
+        for (let index = 0; index < vals.length; index++) {
+          if(vals[index][0] === row) {
+            for (let i = 0; i < this.NUMBER_OF_DAYS; i++) {
+              if(!(this.isWeekend(vals[index][1][i - 1]) === '')) {
+                vals[index][1][i - 1] = 1;
+              }
+              else {
+                vals[index][1][i - 1] = 0;
+              }
+            }
+            break;
+          }
+        }
+        this.values = vals;
+      },
+
+      countValues(row) {
+        let c = 0;
+        for (let index = 0; index < this.values.length; index++) {
+          if(this.values[index][0] === row) {
+            for (let i = 0; i < this.NUMBER_OF_DAYS; i++) {
+              if(this.getValue(row, i)) {
+                c = c + this.getValue(row, i);
+              }
+            }
+            return c;
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .gray-cell {
+    background-color: gray;
+  }
+
+  .selected-cell {
+    background-color: greenyellow;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
 new Vue({
+  vuetify: new Vuetify(),
   render: h => h(App),
 }).$mount('#app')
